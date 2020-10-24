a07b56ed8689010eb8cd4bc278d9b8319fc8e74c
Madushan Sandaruwan <47908590+MadushanSandaru1@users.noreply.github.com>
2020-10-25 03:52:27 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,29 @@
 function dynamic_color(event) {
 	
-	var x = event.pageX;
-  	var y = event.pageY;
+	let x = event.pageX;
+  	let y = event.pageY;
 
-  	var box = document.getElementById("navbar");
-	var width = box.offsetWidth;
-	var height = box.offsetHeight;
+  	let box = document.getElementById("navbar");
+	let width = box.offsetWidth;
+	let height = box.offsetHeight;
 
 	document.getElementById("navbar").style.background = "radial-gradient(circle at "+Math.round((x/document.body.offsetWidth)*100)+"% "+Math.round((y/document.body.offsetHeight)*100)+"% ,  rgba(39, 71, 186,0.8) 5%, rgba(39, 71, 186,0.1))";
 	//document.getElementById("navbar").style.transition = "background 1s";
 }
 
-dynamic_text
+function team_collapse() {
+    let coll = document.getElementsByClassName("item-div");
+    let i;
 
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("div-active");
+            let content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+}
