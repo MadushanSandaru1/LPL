7b5f1f9fe3d8a413e52660ac4a0d65ab97782880
Ruvindu <ruvindumadushanka@gmail.com>
2020-10-25 02:50:27 +0530

    feedback

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,6 @@ function dynamic_color(event) {
 	document.getElementById("navbar").style.background = "radial-gradient(circle at "+Math.round((x/document.body.offsetWidth)*100)+"% "+Math.round((y/document.body.offsetHeight)*100)+"% ,  rgba(39, 71, 186,0.8) 5%, rgba(39, 71, 186,0.1))";
 	//document.getElementById("navbar").style.transition = "background 1s";
 }
+
+dynamic_text
+
